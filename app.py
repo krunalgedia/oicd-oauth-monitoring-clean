d97018e020278b18ef94a41edbdedd09a3e8ddef
krunalgedia <kbgedia24@gmail.com>
2026-03-29 20:36:41 +0200

    up

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/api/v1')
+# Change this back to just '/'
+@app.route('/')
 def hello():
-    # 1. Grab the name tag Apigee pinned to the request!
-    # If the header is missing for some reason, it defaults to 'Unknown User'
     user_email = request.headers.get('X-User-Email', 'Unknown User')
     
-    # 2. Log it securely to Google Cloud Logging
+    # Log it securely to Google Cloud Logging
     print(f"SECURITY LOG: API accessed successfully by {user_email}")
     
-    # 3. Send a custom response back to the user
+    # Send a custom response back to the user
     return f"Hello {user_email} from Cloud Run!\n"
 
 if __name__ == '__main__':
